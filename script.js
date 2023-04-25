cec8eab79e1e161d8c5fd97001e48a786993cef9
Philip Thomsen <philip-th@live.dk>
2023-04-25 13:59:06 +0200

    Update

--- script.js
+++ script.js
@@ -5,13 +5,25 @@ const words = [
     ["Jak", "How"],
     ["Ja", "I"],
 ];
-let Arraylength = words.length;
-let x = words[Math.floor((Math.random()*words.length))];
-let value = x[0]
-let value2 = x[1]
 
-console.log();
+const word = [
+    {
+        polish: "Badzo",
+        english: "Very",
+        sentenceEnglish: "That is very good",
+        sentencePolish: "To jest badzo dobre"
 
-document.getElementById('word').innerHTML = value;
-document.getElementById('bottom').innerHTML = value2;
+    }
+]
+let today = new Date();
+let startOfYear = new Date(today.getFullYear(), 0, 0);
+let diff = (today - startOfYear) + ((startOfYear.getTimezoneOffset() - today.getTimezoneOffset()) * 60 * 1000);
+let oneDay = 1000 * 60 * 60 * 24;
+let dayOfYear = Math.floor(diff / oneDay);
+
+console.log(dayOfYear);
+
+
+document.getElementById('word').innerHTML = word[0].polish;
+document.getElementById('bottom').innerHTML = word[0].english;
 
